ec22b3cc328979e531b3e613dd0b877d2d6e20fb
mei-riko <dobrovolska.marina@gmail.com>
2021-11-30 16:15:50 +0300

    fonts & new page

--- src/items/input/input.js
+++ src/items/input/input.js
@@ -1,5 +1,39 @@
 import $ from 'jquery';
 
+function validateEmail($email) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,5})?$/;
+    return emailReg.test( $email );
+}
+function checkInputValue( $input ){
+	let name = $input.attr('data-input');
+		
+	switch (name) {
+		case 'message':
+			if( $input.val().length > 2 ){
+				$input.removeClass('input--error').addClass('input--success');
+			}else{
+				$input.removeClass('input--success').addClass('input--error');
+			}
+			break;
+		case 'mail':
+			if( validateEmail($input.val()) &&  $input.val().length > 0 ){
+				$input.removeClass('input--error').addClass('input--success');
+			}else{
+				$input.removeClass('input--success').addClass('input--error');
+			}
+			break;
+		case 'name':
+			if( $input.val().length > 2 ){
+				$input.removeClass('input--error').addClass('input--success');
+			}else{
+				$input.removeClass('input--success').addClass('input--error');
+			}
+			break;
+		default:
+			break;
+	};
+}
+
 export let unitMaskPhone = () => {
     $('input[data-input="phone"]').inputmask({
         mask: '+7 (999) 999 99 99',
@@ -25,6 +59,17 @@ $(function() {
     if( $('input[type="tel"]').length > 0 ) {
         unitMaskPhone();
     }
+
+    // Проверка для смены классов при введении
+	$('.input').on('input', function() {
+		let $input = $(this);
+		checkInputValue( $input );
+	});
+	// Проверка для смены классов при автозаполнении
+	$('.input').on('change', function() {
+		let $input = $(this);
+		checkInputValue( $input );
+	});
 });
 
 $('input').on('keydown', function(event) {
